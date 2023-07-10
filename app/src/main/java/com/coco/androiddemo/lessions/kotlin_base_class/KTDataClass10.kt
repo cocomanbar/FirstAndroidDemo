d9c82522b261016fd104ca74a9161f3364ebf445
tanxl <125322078@qq.com>
2023-07-10 21:42:37 +0800

    嵌套类与内部类

--- app/src/main/java/com/coco/androiddemo/lessions/kotlin_base_class/KTDataClass10.kt
+++ app/src/main/java/com/coco/androiddemo/lessions/kotlin_base_class/KTDataClass10.kt
@@ -20,12 +20,14 @@ class Tester1 {
 
 // 标记为 inner 的嵌套类能够访问其外部类的成员。内部类会带有一个对外部类的对象的引用
 class Tester2 {
-    private val bar1: Int = 1
+    val bar1: Int = 1
+
     inner class Nedsted2 {
-        private val bar2: Int = 1
+        private var bar2: Int = 1
         fun test(): Unit {
             println("${this.bar2}")
-            println("${super@Tester2.bar1}")
+            bar2 = bar1
+            println("$bar1")
         }
     }
 }
