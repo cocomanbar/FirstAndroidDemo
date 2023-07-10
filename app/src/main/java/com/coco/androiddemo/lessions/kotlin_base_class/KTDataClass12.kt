32b240b99843f740b12c62eca1662390df8a0622
tanxl <125322078@qq.com>
2023-07-10 22:18:53 +0800

    类型别名

--- app/src/main/java/com/coco/androiddemo/lessions/kotlin_base_class/KTDataClass12.kt
+++ app/src/main/java/com/coco/androiddemo/lessions/kotlin_base_class/KTDataClass12.kt
@@ -1,15 +1,25 @@
 package com.coco.androiddemo.lessions.kotlin_base_class
 
-// 对象表达式与对象声明
-
 fun main() {
 
     val ab: Aer = object : Aer(1), Ber {
 
     }
 
+    // 类型别名
+    // 类似函数嵌套函数？
+    val f: (Int) -> Boolean = {
+        it > 0
+    }
+    println(foo(f))
+
+    val p: Predicate<Int> = {
+        it > 0
+    }
+    println(listOf(1, -2).filter(p))
 }
 
+// 1.对象表达式与对象声明
 
 open class Aer(val x: Int) {
     public open val y: Int = x
@@ -27,3 +37,42 @@ fun test() {
     }
     println(adHot.x + adHot.y)
 }
+
+// 2.类别别名
+
+typealias IntSet = Set<Int>
+
+typealias Handler = (Int, String) -> Unit
+
+
+class Aer1 {
+    inner class Ber {
+
+    }
+}
+
+typealias ABer = Aer1.Ber
+
+// 在代码中添加 typealias Predicate<T> 并使用 Predicate<Int> 时，
+// Kotlin 编译器总是把它扩展为 (Int) -> Boolean
+
+typealias Predicate<T> = (T) -> Boolean
+
+fun foo(p: Predicate<Int>) = p(42)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
