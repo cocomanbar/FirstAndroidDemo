6e9089eaa5baf3523a20ad0382b223563270ebb7
tanxl <125322078@qq.com>
2023-07-12 22:15:53 +0800

    高阶函数与 lambda 表达式

---
+++ app/src/main/java/com/coco/androiddemo/lessions/functions/KTFunction2.kt
@@ -0,0 +1,51 @@
+package com.coco.androiddemo.lessions.functions
+
+// 高阶函数与 lambda 表达式
+
+// Kotlin 函数都是头等的，这意味着它们可以存储在变量与数据结构中、作为参数传递给其他高阶函数以及从其他高阶函数返回。
+// 可以像操作任何其他非函数值一样操作函数
+
+
+// 高阶函数是将函数用作参数或返回值的函数
+
+
+// 函数类型
+// (A, B) -> C
+// () -> C
+// () -> Unit
+
+// ((Int, Int) -> Int)?
+
+// typealias ClickHandler = (Button, ClickEvent) -> Unit
+
+
+// Lambda 表达式与匿名函数
+// val sum: (Int, Int) -> Int = { x: Int, y: Int -> x + y }
+
+// 单个参数的隐式名称
+
+
+// 闭包
+
+//var sum = 0
+//ints.filter { it > 0 }.forEach {
+//    sum += it
+//}
+//print(sum)
+
+// 内联函数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
