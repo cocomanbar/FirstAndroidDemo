fdc26bbf86b73863defbd720424b5925d25b3d94
tanxl <125322078@qq.com>
2023-07-21 23:22:57 +0800

    Retrofit

--- app/src/main/java/com/coco/androiddemo/MainActivity.kt
+++ app/src/main/java/com/coco/androiddemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.coco.androiddemo
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,7 +9,12 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.coco.androiddemo.databinding.ActivityMainBinding
+import com.coco.androiddemo.network.ApiServer
 import com.coco.androiddemo.network.HiOkHttp
+import com.coco.androiddemo.network.HiRetrofit
+import com.coco.androiddemo.network.UserJson
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,10 +27,18 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         // 测试网络请求
-        testNetwork()
+//        testHiHttp()
+
+        testRetrofit()
+
     }
 
-    private fun testNetwork() {
+
+    private fun testHiHttp() {
+
+        //  onFailure
+        //  onResponse
+        //  回调在子线程
 
          HiOkHttp.networkAsyncGet("user/query?userId=1600932269")
 
@@ -33,5 +47,22 @@ class MainActivity : AppCompatActivity() {
         // HiOkHttp.networkAsyncPostJSONString()
 
         // HiOkHttp.networkAsyncPostString()
+    }
+
+    private  fun testRetrofit() {
+
+        //  onFailure
+        //  onResponse
+        //  回调在主线程
+
+        val apiServer = HiRetrofit.create(ApiServer::class.java)
+        apiServer.queryUser("160092369").enqueue(object : retrofit2.Callback<UserJson>{
+            override fun onFailure(call: Call<UserJson>, t: Throwable) {
+                Log.e("Retrofit", "${t.message ?: "onFailure"}")
+            }
+            override fun onResponse(call: Call<UserJson>, response: Response<UserJson>) {
+                Log.e("Retrofit", response.body()?.toString() ?: "onResponse")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/coco/androiddemo/network/HiRetrofit.kt
@@ -0,0 +1,84 @@
+package com.coco.androiddemo.network
+
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.FieldMap
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.PartMap
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+import java.util.concurrent.TimeUnit
+
+
+// RESTFUL网络请求框架Retrofit
+// https://github.com/square/retrofit
+
+object HiRetrofit {
+
+    private const val base_url: String = "http://123.56.232.18:8080/serverdemo/"
+
+    private  val httpLoggingInterceptor = LoggingInterceptor()
+
+    private val client = OkHttpClient.Builder()  // 构造器
+    .connectTimeout(15, TimeUnit.SECONDS)   // 连接超时
+    .readTimeout(15, TimeUnit.SECONDS)      // 读取超时
+    .writeTimeout(15, TimeUnit.SECONDS)     // 写超时
+    .addInterceptor(httpLoggingInterceptor)         // 网络监听
+    .build()
+
+    private var retrofit: Retrofit = Retrofit.Builder()
+        .client(client)
+        .baseUrl("$base_url")
+        .addConverterFactory(GsonConverterFactory.create())     //数据转换器
+        .build()
+
+    fun <T> create(clazz: Class<T>): T {
+        return retrofit.create(clazz)
+    }
+}
+
+
+interface ApiServer {
+
+    @GET(value = "user/query")
+    fun queryUser(@Query(value = "userId", encoded = true) userId: String): Call<UserJson>
+
+    // 使用@Headers添加多个请求头
+    @Headers("{ \"X-Foo: Bar\", \"X-Ping: Pong\" }")
+    @GET(value = "user/query")
+    fun queryUser1(@Query(value = "suerId", encoded = true) userId: String): Call<UserJson>
+
+    // 多个参数的情况下可以使用@QueryMap
+    @GET(value = "user/query")
+    fun queryUser2(@QueryMap map: Map<String, String>): Call<UserJson>
+
+    /*
+    *   很多情况下，需要上传json格式，而且数据很多，还会变化，这个时候可以使用 @Body
+    *   字符串提交
+    */
+    @POST(value = "user/update")
+    fun userUpdate(@Body body: RequestBody)
+
+    // 表单提交
+    @POST(value = "user/update")
+    @FormUrlEncoded
+    fun userUpdate1(@FieldMap map: Map<String, Object>): Call<Unit>
+
+    // 表单上传文件（键值对提交，同时上传文件）
+    @POST(value = "upload/upload")
+    @Multipart
+    fun upload(@Part file: RequestBody, @Query(value = "openId", encoded = true) openId: String, @PartMap map: Map<String, RequestBody>): Call<Unit>
+
+
+
+}
+
